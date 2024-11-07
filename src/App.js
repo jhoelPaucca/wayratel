c8b3e27ce3fce0437cbf01ad43116cbc1a4e9fcd
jhoelDevs <pauccajhoel@gmail.com>
2024-11-07 07:49:47 -0600

    actualizacion

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> 
+        <h1>Wayratel Empresas</h1> 
+        <p>Bienvenido a la plataforma de Wayratel. ¡Estamos aquí para ayudarte a crecer!</p> 
+        <button className="App-button">Comenzar</button> 
+      </header>
+    </div>
+    
+  );
+}
+
+export default App;
