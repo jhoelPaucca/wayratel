4df1c357f61f237bc3fdf3bb2f7f74ed43e90090
jhoelDevs <pauccajhoel@gmail.com>
2024-11-07 07:56:06 -0600

    new dialogo

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <header className="App-header"> 
         <h1>Wayratel Empresas</h1> 
         <p>Bienvenido a la plataforma de Wayratel. ¡Estamos aquí para ayudarte a crecer!</p> 
+        <p>Como estas mi gente de google</p>
         <button className="App-button">Comenzar</button> 
       </header>
     </div>
